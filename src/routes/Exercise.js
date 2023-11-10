46928b158516303462911a54b8935b379b4e8a9c
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-11-10 20:33:17 +0530

    fetching all exercise end point created

--- src/routes/Exercise.js
+++ src/routes/Exercise.js
@@ -2,31 +2,42 @@ const express = require('express');
 const router = express.Router();
 const { Exercise } = require('../models/Exercise');
 
-// router.get('/', getExercises);
+// PRIVET ACCESS - X
+
+// Fetched all exercises 
+router.get('/', async (req, res) => {
+    const fetchedAllExercises = await Exercise.findAll();
+    try {
+        exercisesJSON = fetchedAllExercises.map(exercise => exercise.toJSON());
+        res.send(exercisesJSON);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+// Fetched Exercise by primary key
 router.get('/:exerciseID', async (req, res) => {
     const exerciseID = req.params.exerciseID; 
     console.log(exerciseID);
     const fetchedExercise = await Exercise.findByPk(exerciseID);
     try {
         res.send(fetchedExercise.toJSON());
-    }
-    catch(err) {
+    } catch(err) {
         res.send(err);
     }
 });
 
+// Add new exercise
 router.post('/', async (req, res) => {
     const exercise = await Exercise.create({name: req.body.name, description: req.body.description}); 
     try {
         res.send(exercise.toJSON());
-    }
-    catch(err) {
+    } catch(err) {
         res.send(err);
     }
 });
 
 module.exports = { exerciseRouter: router }
 
-// router.delete('/:exerciseID', deleteExercise);
 
 // router.put('/:exerciseID', updateExercise)
