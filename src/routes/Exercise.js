a83e96f80767c1801fd54555521a9a039787a02c
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-11-10 15:01:51 +0530

    fetched exercise end point created

--- src/routes/Exercise.js
+++ src/routes/Exercise.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const { Exercise } = require('../models/Exercise');
 
 // router.get('/', getExercises);
-
+router.get('/:exerciseID', async (req, res) => {
+    const exerciseID = req.params.exerciseID; 
+    console.log(exerciseID);
+    const fetchedExercise = await Exercise.findByPk(exerciseID);
+    try {
+        res.send(fetchedExercise.toJSON());
+    }
+    catch(err) {
+        res.send(err);
+    }
+});
 
 router.post('/', async (req, res) => {
     const exercise = await Exercise.create({name: req.body.name, description: req.body.description}); 
