3bff0d78eb5a4675993cb2f4db42aa307a797e34
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-11-17 22:58:11 +0530

    association built

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,11 +1,13 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../db/db');
+const { Exercise } = require('../models/Exercise');
 
-const Category = sequelize.define('Categorie', {
+const Category = sequelize.define('Category', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
-        primaryKey: true
+        primaryKey: true,
+        allowNull: false
     },
     name: {
         type: DataTypes.STRING,
@@ -16,5 +18,10 @@ const Category = sequelize.define('Categorie', {
         type: DataTypes.STRING
     }
 });
+
+console.log(sequelize.models);
+
+Category.hasMany( sequelize.models.Exercise, {foreignKey: 'cid'} );
+Exercise.belongsTo(Category, { foreignKey: 'cid' })
 
 module.exports = { Category };
